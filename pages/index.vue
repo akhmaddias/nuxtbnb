6c05c12a68608e85a130392212ce38837c448a42
Akhmad Dias <akhmad.dias94@gmail.com>
2024-10-18 17:49:01 +0500

    move homecard to separate component

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
     <div v-for="home in homes" :key="home.objectID">
-      <img :src="home.images[0]" style="width: 200px"> <br />
-      {{ home.title }} <br />
-      {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} <br />
-      {{ home.guests }} guests <br />
-      {{ home.pricePerNight }} / night <br />
+      <home-card :home="home" />
     </div>
   </div>
 </template>
